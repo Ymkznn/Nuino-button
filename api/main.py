7c5826fad6dd1bdaf0ada9534142d211497c1be9
ymkznn <lsm871008@gmail.com>
2024-02-20 12:48:21 +0800

    audio

--- api/main.py
+++ api/main.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template,request
 app = Flask(__name__)
 
 # 假設你的 JSON 數據
-json_data = {
-    "buttons": ["Button 1", "Button 2", "Button 3", "Button 4"]
+button_audio_mapping = {
+    "Button 1": "audio1.mp3",
+    "Button 2": "audio2.mp3",
+    "Button 3": "audio3.mp3"
 }
 
 # 路由，根據設備返回不同的 HTML 模板
@@ -13,9 +15,9 @@ def index():
     # 檢測用戶訪問設備，這裡簡單地假設是桌面電腦
     agent = request.headers.get('User-Agent')
     if any(device in agent.lower() for device in ('iphone', 'android', 'blackberry')):
-            return render_template('index_mobile.html', buttons=json_data['buttons'])
+            return render_template('index_mobile.html', button_audio_mapping=button_audio_mapping)
     else:
-        return render_template('index_desktop.html', buttons=json_data['buttons'])
+        return render_template('index_desktop.html', button_audio_mapping=button_audio_mapping)
 
 if __name__ == '__main__':
     app.run(debug=True)
