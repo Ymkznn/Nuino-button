10a4f9607cd122a8e4697103be2fceae71458bf0
ymkznn <lsm871008@gmail.com>
2024-02-20 11:31:26 +0800

    first commit

---
+++ mainweb.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+# 假設你的 JSON 數據
+json_data = {
+    "buttons": ["Button 1", "Button 2", "Button 3"]
+}
+
+# 路由，根據設備返回不同的 HTML 模板
+@app.route('/')
+def index():
+    # 檢測用戶訪問設備，這裡簡單地假設是桌面電腦
+    agent = request.headers.get('User-Agent')
+    if any(device in agent.lower() for device in ('iphone', 'android', 'blackberry')):
+            return render_template('index_mobile.html', buttons=json_data['buttons'])
+    else:
+        return render_template('index_desktop.html', buttons=json_data['buttons'])
+
+if __name__ == '__main__':
+    app.run(host='192.168.8.130',debug=True)
